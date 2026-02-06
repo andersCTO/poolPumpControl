b9fa227e26a7f3a432ffaf3ed7111b5b3b8e8d48
Anders Kullenberg <anderskullenberg@outlook.com>
2026-02-06 08:04:55 +0100

    Fix esptool deprecation warnings in flash.bat

--- flash.bat
+++ flash.bat
@@ -7,7 +7,7 @@ set PORT=%1
 if "%PORT%"=="" set PORT=COM3
 
 echo Flashing to %PORT%...
-python -m esptool --chip esp32 -p %PORT% -b 460800 --before default_reset --after hard_reset write_flash --flash_mode dio --flash_size 4MB --flash_freq 40m 0x1000 \\wsl$\Ubuntu\home\ankullen\development\poolPumpControl\build\bootloader\bootloader.bin 0x8000 \\wsl$\Ubuntu\home\ankullen\development\poolPumpControl\build\partition_table\partition-table.bin 0x10000 \\wsl$\Ubuntu\home\ankullen\development\poolPumpControl\build\pool_pump_controller.bin
+python -m esptool --chip esp32 -p %PORT% -b 460800 --before default-reset --after hard-reset write-flash --flash-mode dio --flash-size 4MB --flash-freq 40m 0x1000 \\wsl$\Ubuntu\home\ankullen\development\poolPumpControl\build\bootloader\bootloader.bin 0x8000 \\wsl$\Ubuntu\home\ankullen\development\poolPumpControl\build\partition_table\partition-table.bin 0x10000 \\wsl$\Ubuntu\home\ankullen\development\poolPumpControl\build\pool_pump_controller.bin
 
 if %ERRORLEVEL% EQU 0 (
     echo.
